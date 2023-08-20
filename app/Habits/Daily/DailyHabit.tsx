c11afe9c845015563747a4b958202e66003eca2f
Johannes Ströbele <johannes.stroebele@partner.bmwgroup.com>
2023-08-20 16:59:32 +0200

    Add function for daily habits to handle setting a habit done, todo, and editing it

--- app/Habits/Daily/DailyHabit.tsx
+++ app/Habits/Daily/DailyHabit.tsx
@@ -1,31 +1,52 @@
 "use client";
 import {CheckOutlined, EditOutlined, RedoOutlined} from "@ant-design/icons";
-import {Button, Card, Col, Row} from "antd";
+import {Button, Card, Col, Row, Typography} from "antd";
 import React from "react";
 import {Habit} from "@/app/types";
+
+const {Text} = Typography;
 
 interface DailyHabitProps {
     habit: Habit
 }
 
 export const DailyHabit = ({habit}: DailyHabitProps) => {
+
+    // TODO write logic to mutate habits in the database
+    function setHabitDone(habit: Habit) {
+        console.log(habit)
+        return;
+    }
+
+    function setHabitTodo(habit: Habit) {
+        console.log(habit)
+        return;
+    }
+
+    function editHabit(habit: Habit) {
+        console.log(habit)
+        return;
+
+    }
+
     const Buttons = (): React.JSX.Element => {
         const isDone = habit.doneHistory && habit.doneHistory.length > 0 && habit.doneHistory.some(date => new Date(date).toDateString() === new Date().toDateString())
         if (isDone) {
             return (<Row>
-                <Col span={12}>
-                    <Button><CheckOutlined style={{color: 'blue'}}/>Completed</Button>
+                <Col span={12} style={{lineHeight: 2.3}}>
+                    <Text><CheckOutlined
+                        style={{color: 'blue', marginRight: 5}}/>Completed</Text>
                 </Col>
                 <Col span={12} style={{textAlign: 'right'}}>
-                    <Button><RedoOutlined style={{color: 'blue'}}/>Undo</Button>
+                    <Button onClick={() => setHabitTodo(habit)}><RedoOutlined style={{color: 'blue'}}/>Undo</Button>
                 </Col>
             </Row>)
         }
-        return <Button type="primary">Mark as complete</Button>
+        return <Button type="primary" onClick={() => setHabitDone(habit)}>Mark as complete</Button>
     }
 
     return <Col>
-        <Card title={habit.name} size="small" extra={<EditOutlined/>}>
+        <Card title={habit.name} size="small" extra={<EditOutlined onClick={() => editHabit(habit)}/>}>
             <Buttons/>
         </Card>
     </Col>;
